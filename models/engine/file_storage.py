ed0ce8b76873bd46f5f649611bd0743e1e00ba4f
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-05-10 06:53:00 +0000

    Update file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,3 +2,29 @@
 """File Storage class rising from models"""
 from json import loads, dump, dumps
 from os.path import exists
+from models import base_model
+from models import user
+from models import place
+from models import state
+from models import city
+from models import amenity
+from models import review
+
+City = city.City
+Amenity = amenity.Amenity
+Place = place.Place
+BaseModel = base_model.BaseModel
+User = user.User
+State = state.State
+Review = review.Review
+
+name_class = ["BaseModel", "City", "State", "Place", "Amenity", "Review", "User"]
+
+class FileStorage:
+    """
+    Class for the file storage
+    File used file.json
+    Begins with empty obj
+    """
+    __file_path = "file.json"
+    __objects = {}
