5cbe6bf65ee818f113a58d8a2098c5605967f138
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-05-10 20:10:01 +0000

    Update file_storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,3 +28,21 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
+    def new(self, objs):
+        """
+        Sets the object with key into __objects{}
+        """
+        ind = "{}.{}".format(objs.__class__.__name__, objs.id)
+        FileStorage.__objects[key] = objs
+    def reload(self):
+        """
+        Deserialze json to __objects
+        """
+        if exist(FileStorage.__file_path):
+            #Done if json file exists, recorded in file_path
+            with open(FileStorage.__file_path, "r", encoding='utf-8') as doc:
+                dict_from_j = load(doc)
+            for key, value in dict_from_j.items():
+                if key.split('.')[0] in name_class:
+                    #Deserialize indeces
+                    FileStorage.__objects[key] = eval(key.split('.')[0])(**value)
