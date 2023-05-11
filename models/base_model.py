aea79f04112e569c65aee9655de8da72622baafe
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-05-11 22:14:18 +0000

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,11 @@
 Modules for base model.
 """
 
-import uuid
 from datetime import datetime
+import json
+import uuid
+import models
+
 
 class BaseModel:
 
@@ -13,25 +16,51 @@ class BaseModel:
     class that defines all common attributes and methods for other classes.
     """
 
-    def __init__(self):
-        """
-        Initialization of new instance of base class.
-        """
+    def __init__(self, **kwargs):
+        """Initialize a new BaseModel instance with the given attributes."""
+
+        # Set a unique ID for the instance
         self.id = str(uuid.uuid4())
+
+        # Set the creation and update dates to the current date and time
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+        # Set the date string format
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+
+        # Update the instance attributes based on the given keyword arguments
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime(v, tform))
+                else:
+                    setattr(self, k, v)
+        else:
+            models.storage.new(self)
+
     def __str__(self):
-        """
-        Returns string presentation of the instance of the  base model.
-        """
-        return "[{}] ({}) {}".format(type(self.__name__, self.id, self.__dict__)
+        """Return the print/str representation of the BaseModel instance."""
+        # Get the class name and instance ID
+        clname = type(self).__name__
+        instance_id = getattr(self, "id", None)
+        # Get a dictionary of the instance attributes
+        instance_attrs = vars(self).copy()
+        # Remove any reserved attributes
+        reserved_attrs = ["id", "created_at", "updated_at"]
+        for attr in reserved_attrs:
+            instance_attrs.pop(attr, None)
+        # Format the string representation of the instance
+        instance_attrs_str = ", ".join(f"{k}={v!r}" for k, v in instance_attrs.items())
+        instance_str = f"[{clname}] ({instance_id}) {{{instance_attrs_str}}}"
+        return instance_str
 
     def save(self):
         """
         Updates the attribute 'update_at' to current date and time.
         """
         self.update_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
